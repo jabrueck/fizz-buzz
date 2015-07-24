5836f7de1dae9ba2cce0ea5373d749705c59620c
Josh <jabrueck@gmail.com>
2015-07-23 21:43:44 -0500

    fizz buzz refactor - switched to jquery and added user number input. Fixed fizzbuzz at 15's

--- scriptz.js
+++ scriptz.js
@@ -1,4 +1,4 @@
-for (var i=1; i < 101; i++) {
+/*for(var i=1; i < 101; i++) {
 	if (i % 3 == 0) {
 		document.write("Fizz");
 		document.write("<br/>");
@@ -10,3 +10,20 @@ for (var i=1; i < 101; i++) {
 		document.write("<br />");
 	}
 }
+-- forgot to account for the 15 and fizzbuzz */
+
+$( document ).ready(function() {
+	var input = +`${prompt("enter a number")}`;
+	for (var i=1; i <= input; i++) {
+		if (i % 15 == 0) {
+			$("#count").append("fizzbuzz");
+		} else if (i % 3 == 0) {
+			$("#count").append("fizz");
+		} else if (i % 5 == 0) {
+			$("#count").append("buzz");
+		} else {
+			$("#count").append(i);
+		}
+		$("#count").append("<br>");
+  	}
+});
